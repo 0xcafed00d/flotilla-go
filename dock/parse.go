47d4e54deba0fe6b9ca219134e439d7d7b136601
Lee Witek <neomech@gmail.com>
2016-03-18 23:52:45 +0000

    added msgToEvent

--- dock/parse.go
+++ dock/parse.go
@@ -1,6 +1,9 @@
 package dock
 
-import "bytes"
+import (
+	"bytes"
+	"fmt"
+)
 
 func makeMessageSplitter() func(input []byte) []string {
 	buffer := []byte{}
@@ -22,6 +25,35 @@ func makeMessageSplitter() func(input []byte) []string {
 	}
 }
 
+// u 1/module int,int,int,int....
 func msgToEvent(msg string) Event {
-	return Event{}
+	var evtype rune
+	var port int
+	var module string
+	var values [8]int
+
+	n, _ := fmt.Sscanf(msg, "%c %d/%s %d,%d,%d,%d,%d,%d,%d,%d",
+		&evtype, &port, &module,
+		&values[0], &values[1], &values[2], &values[3],
+		&values[4], &values[5], &values[6], &values[7])
+
+	//	fmt.Println(evtype, port, module, values, n, err)
+
+	event := Event{}
+	switch evtype {
+	case 'c':
+		event.EventType = Connected
+	case 'd':
+		event.EventType = Disconnected
+	case 'u':
+		event.EventType = Update
+	default:
+		event.EventType = Invalid
+	}
+
+	event.ModuleType = FromString(module)
+	event.Port = port
+	event.Params = values[:n-3]
+
+	return event
 }
