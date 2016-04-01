797ea4bb6e5974313b262f7a388d81338629b76a
Lee Witek <neomech@gmail.com>
2016-04-01 01:45:31 +0100

    use a module adress to locate the module

--- flotilla/module.go
+++ flotilla/module.go
@@ -2,23 +2,27 @@ package flotilla
 
 import "github.com/simulatedsimian/flotilla/dock"
 
+type ModuleAddress struct {
+	dock, index int
+}
+
 type Module struct {
+	ModuleAddress
 	dock.ModuleType
-	*dock.Dock
-	index int
+	*Client
 }
 
 func (m *Module) Update(ev Event) {
 	if ev.ModuleType == m.ModuleType {
 		if ev.EventType == dock.Connected {
-			m.index = ev.Port
+			m.ModuleAddress = ModuleAddress{ev.dockIndex, ev.Port}
 		}
 		if ev.EventType == dock.Disconnected {
-			m.index = -1
+			m.ModuleAddress = ModuleAddress{-1, -1}
 		}
 	}
 }
 
 func (m *Module) Connected(ev Event) bool {
-	return m.index != -1
+	return m.ModuleAddress.index != -1
 }
