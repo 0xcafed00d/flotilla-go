70d46230ca0062fe90b01a58426c7eb9503dca26
Lee Witek <neomech@gmail.com>
2016-04-08 00:35:41 +0100

    fix dock test and after field renames

--- dock/dock_test.go
+++ dock/dock_test.go
@@ -40,7 +40,7 @@ func TestValidation(t *testing.T) {
 func TestMsgSplitter(t *testing.T) {
 	assert := assert.Make(t)
 
-	msgSplit := makeMessageSplitter()
+	msgSplit := makeMessageSplitter([]byte{0x0d, 0x0a})
 
 	assert(msgSplit([]byte("msg1\r\nmsg2\r\n"))).Equal([]string{"msg1", "msg2"})
 	assert(msgSplit([]byte("msg3\r\nmsg"))).Equal([]string{"msg3"})
@@ -53,20 +53,20 @@ func TestMsgParser(t *testing.T) {
 	assert(msgToEvent("u 1/joystick 1,234,874")).Equal(Event{
 		EventType:  Update,
 		ModuleType: Joystick,
-		Port:       1,
+		Channel:    1,
 		Params:     []int{1, 234, 874},
 	})
 
 	assert(msgToEvent("c 1/joystick")).Equal(Event{
 		EventType:  Connected,
 		ModuleType: Joystick,
-		Port:       1,
+		Channel:    1,
 	})
 
 	assert(msgToEvent("d 1/joystick")).Equal(Event{
 		EventType:  Disconnected,
 		ModuleType: Joystick,
-		Port:       1,
+		Channel:    1,
 	})
 
 	assert(msgToEvent("# this is a message")).Equal(Event{
@@ -77,14 +77,14 @@ func TestMsgParser(t *testing.T) {
 	assert(msgToEvent("u 1/xxxx 1,234,874")).Equal(Event{
 		EventType:  Update,
 		ModuleType: Unknown,
-		Port:       1,
+		Channel:    1,
 		Params:     []int{1, 234, 874},
 	})
 
 	assert(msgToEvent("p 1/xxxx 1,234,874")).Equal(Event{
 		EventType:  Invalid,
 		ModuleType: Unknown,
-		Port:       1,
+		Channel:    1,
 		Params:     []int{1, 234, 874},
 	})
 }
@@ -108,20 +108,20 @@ func TestMsgRec(t *testing.T) {
 	assert(<-d.Events).Equal(Event{
 		EventType:  Connected,
 		ModuleType: Joystick,
-		Port:       1,
+		Channel:    1,
 	})
 
 	assert(<-d.Events).Equal(Event{
 		EventType:  Update,
 		ModuleType: Joystick,
-		Port:       1,
+		Channel:    1,
 		Params:     []int{1, 234, 874},
 	})
 
 	assert(<-d.Events).Equal(Event{
 		EventType:  Disconnected,
 		ModuleType: Joystick,
-		Port:       1,
+		Channel:    1,
 	})
 
 	assert(<-d.Events).Equal(Event{
