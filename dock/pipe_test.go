fba111fa4df2f7566eadf1945392b508fd8df714
Lee Witek <neomech@gmail.com>
2016-04-11 22:48:44 +0100

    added test for close

--- dock/pipe_test.go
+++ dock/pipe_test.go
@@ -2,11 +2,27 @@ package dock
 
 import (
 	"fmt"
+	"io"
 	"testing"
 	"time"
 
 	"github.com/simulatedsimian/assert"
 )
+
+func TestClose(t *testing.T) {
+	assert := assert.Make(t)
+
+	buffer := make([]byte, 128)
+
+	e1, e2, _ := NewPipe()
+
+	assert(fmt.Fprintf(e1, "hello")).Equal(5, nil)
+	assert(fmt.Fprintf(e1, "world")).Equal(5, nil)
+	e1.Close()
+
+	assert(e2.Read(buffer)).Equal(10, io.EOF)
+	assert(e2.Read(buffer)).Equal(0, io.EOF)
+}
 
 func TestPipe1(t *testing.T) {
 	assert := assert.Make(t)
