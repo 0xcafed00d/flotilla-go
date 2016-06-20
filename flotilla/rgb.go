d5d5bf2445f46a10cdab8346d1e5276a338dfeac
Lee Witek <neomech@gmail.com>
2016-06-20 23:39:02 +0100

    add lerpRGB

--- flotilla/rgb.go
+++ flotilla/rgb.go
@@ -10,3 +10,24 @@ func Blend(rgb1, rgb2 RGB) (rgb RGB) {
 	rgb.B = byte((uint(rgb1.B) + uint(rgb2.B)) / 2)
 	return
 }
+
+func int2byte(a int) byte {
+	if a > 255 {
+		return 255
+	}
+	return byte(a)
+}
+
+func Add(rgb1, rgb2 RGB) (rgb RGB) {
+	rgb.R = int2byte((int(rgb1.R) + int(rgb2.R)))
+	rgb.G = int2byte((int(rgb1.G) + int(rgb2.G)))
+	rgb.B = int2byte((int(rgb1.B) + int(rgb2.B)))
+	return
+}
+
+func LerpRGB(rgb1, rgb2 RGB, t float64) (rgb RGB) {
+	rgb.R = byte(LerpFloat(float64(rgb1.R), float64(rgb2.R), t))
+	rgb.G = byte(LerpFloat(float64(rgb1.G), float64(rgb2.G), t))
+	rgb.B = byte(LerpFloat(float64(rgb1.B), float64(rgb2.B), t))
+	return
+}
