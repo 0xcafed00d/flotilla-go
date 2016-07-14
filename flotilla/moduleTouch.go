85cc999d236f8aa93869b55dd54160648b2a07f0
Lee Witek <neomech@gmail.com>
2016-07-14 23:33:57 +0100

    add update function for touch

--- flotilla/moduleTouch.go
+++ flotilla/moduleTouch.go
@@ -4,7 +4,29 @@ import "github.com/simulatedsimian/flotilla-go/dock"
 
 type Touch struct {
 	ModuleCommon
-	state [4]bool
+	state     [4]bool
+	prevState [4]bool
+
+	fupdateDefault UpdateFunc
+	fupdateUser    func(button int, pressed bool)
+}
+
+func (m *Touch) Construct() {
+	m.fupdateDefault = func(params []int) {
+		m.prevState = m.state
+		for i := range params {
+			m.state[i] = (params[i] != 0)
+		}
+
+		if m.fupdateUser != nil {
+			for i := range params {
+				if m.state[i] != m.prevState[i] {
+					m.fupdateUser(i, m.state[i])
+				}
+			}
+		}
+	}
+	m.OnUpdate(m.fupdateDefault)
 }
 
 func (m *Touch) Type() dock.ModuleType {
@@ -12,14 +34,7 @@ func (m *Touch) Type() dock.ModuleType {
 }
 
 func (m *Touch) OnChange(f func(button int, pressed bool)) {
-	m.OnUpdate(func(params []int) {
-		for i := range params {
-			if m.state[i] != (params[i] != 0) {
-				m.state[i] = (params[i] != 0)
-				f(i, (params[i] != 0))
-			}
-		}
-	})
+	m.fupdateUser = f
 }
 
 func (m *Touch) GetValue() [4]bool {
